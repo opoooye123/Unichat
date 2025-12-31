13f57104d28fc3a975d5cf7e07db05a952ab2888
kolade247 <amolehabeeblah@kolade247s-MacBook-Pro.local>
2025-12-31 20:31:07 +0100

    current change

--- unichat-frontend/src/utils/api.ts
+++ unichat-frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://unichat-5ss8.onrender.com'
+  baseURL: 'https://unichat-5ss8.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
